f88f36c097bf4a765a7adc4e1b83b5865674c70f
Alfonso AG <alfareiza@gmail.com>
2020-05-12 17:57:47 -0300

    General information of this repository

--- Exercises/bomdia_boatarde_boanoite.py
+++ Exercises/bomdia_boatarde_boanoite.py
@@ -1,11 +1,16 @@
+"""
+This program says "bom dia", or "boa tarde" or "boa noite" depending the hour of the moment
+when is executed
+"""
 from datetime import datetime
+
 time = datetime.now()
 def calcula_sauacao(time):
     if 0 < time < 12:
-        return f'Bom dia - {time}:'
+        return f'Bom dia > {time}:'
     elif 12 >= time < 18:
-        return f'Boa tarde - {time}:'
+        return f'Boa tarde > {time}:'
     else:
-        return f'Boa Noite - {time}:'
+        return f'Boa Noite > {time}:'
 
 print(calcula_sauacao(time.hour),time.minute)
