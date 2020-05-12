99f8b43cd835b9a5c598297094c85e6419776214
Alfonso AG <alfareiza@gmail.com>
2020-05-12 18:51:49 -0300

    This file calls to sum_modulo.py sending some data

--- Exercises/sum_call_modulo.py
+++ Exercises/sum_call_modulo.py
@@ -1,5 +1,8 @@
+"""
+This file calls to sum_modulo.py sending some data
+"""
 #from contar_caracteres_list import contar_caracteres as count_str_list
 #from contar_caracteres_dict_v1 import contar_caracteres as count_str_dict
-from Exercises.modulo import sum as s
+from Exercises.sum_modulo import sum as s
 
 print(s(1,2))
