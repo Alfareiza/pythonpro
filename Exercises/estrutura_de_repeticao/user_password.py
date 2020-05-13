96cdb789aa3fa4ef7a78ae548201f4ea1bfbd874
Alfonso AG <alfareiza@gmail.com>
2020-05-12 21:04:44 -0300

    Validates that the password cannot contain the user information.

--- Exercises/estrutura_de_repeticao/user_password.py
+++ Exercises/estrutura_de_repeticao/user_password.py
@@ -1,7 +1,7 @@
 """
 Faça um programa que leia um nome de usuário e a sua senha e não aceite
 a senha igual ao nome do usuário, mostrando uma mensagem de erro e
-voltando a pedir as informações.
+voltando a pedir as informações .
 """
 usuario = input('Usuario: ')
 senha = input('Senha: ')
