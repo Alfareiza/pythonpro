e7c765983f5e599a62976ed5df1d9dfe5f6f96d4
Alfonso AG <alfareiza@gmail.com>
2020-05-12 21:02:57 -0300

    Validates that a number is greater than zero and less than 10

--- Exercises/estrutura_de_repeticao/nota_valida.py
+++ Exercises/estrutura_de_repeticao/nota_valida.py
@@ -1,7 +1,7 @@
 """
 Faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido e
-continue pedindo até que o usuário informe um valor válido.
+continue pedindo até que o usuário informe um valor válido .
 """
 
 point = 1
