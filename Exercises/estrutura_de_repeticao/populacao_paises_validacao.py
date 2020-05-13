c20579d1a71d7bd66297a98bea64e180d5606163
Alfonso AG <alfareiza@gmail.com>
2020-05-12 21:06:04 -0300

    Comparing the growth of two countries with inputs.

--- Exercises/estrutura_de_repeticao/populacao_paises_validacao.py
+++ Exercises/estrutura_de_repeticao/populacao_paises_validacao.py
@@ -2,7 +2,7 @@
  taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com
  uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número
   de anos necessários para que a população do país A ultrapasse ou iguale a população
-  do país B, mantidas as taxas de crescimento."""
+  do país B, mantidas as taxas de crescimento. """
 
 
 def leer_dados():
