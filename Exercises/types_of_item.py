472c13088b7f316d8208f1f704cb33c21b9610ab
Alfonso AG <alfareiza@gmail.com>
2020-05-14 15:12:15 -0300

    Type of items in a list

--- Exercises/types_of_item.py
+++ Exercises/types_of_item.py
@@ -5,5 +5,5 @@ Sample List : datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12
 
 datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
 for i in datalist:
-    print(i,' -> ',type(i))
+    print('type of ', i,' is ',type(i))
 
