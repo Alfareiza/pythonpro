80f9824b9fd442db232154c0c5c78c8712ce91ce
Alfonso AG <alfareiza@gmail.com>
2020-05-15 19:17:29 -0300

    How is called a function with default arguments

--- Examples/Function_examples/call_with_default_arguments.py
+++ Examples/Function_examples/call_with_default_arguments.py
@@ -1,10 +1,11 @@
 # Function definition is here
-def printinfo( name, age = 35 ): #en llamados por default, se hacen este tipo de atribuciones en la definición de la función
-   "This prints a passed info into this function"
-   print ("Name: ", name)
-   print ("Age ", age)
-   return
+def printinfo(name, age=35):  # En llamados por default, se hacen este tipo de atribuciones en la definición de función
+    "This prints a passed info into this function"
+    print("Name: ", name)
+    print("Age ", age)
+    return
+
 
 # Now you can call printinfo function
-printinfo( age = 50, name = "miki" ) # Estos valores son enviados a la función y esta no los cambiará
-printinfo( name = "miki" ) # Como no se envió 'age', la función asumirá que esta es 35 pq ha sido declarada allí
+printinfo(age=50, name="miki")  # Estos valores son enviados a la función y esta no los cambiará
+printinfo(name="miki")  # Como no se envió 'age', la función asumirá que esta es 35 pq ha sido declarada allí
