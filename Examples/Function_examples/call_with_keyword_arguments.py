be74d73b88db7837464831cabcf5e3ad2243d6a5
Alfonso AG <alfareiza@gmail.com>
2020-05-15 19:21:24 -0300

    How is called a function with keyword arguments

--- Examples/Function_examples/call_with_keyword_arguments.py
+++ Examples/Function_examples/call_with_keyword_arguments.py
@@ -1,20 +1,24 @@
 # Function definition is here -----------------------------------
-def printme( str ):
-   "This prints a passed string into this function"
-   print (str)
-   return
+def printme(str):
+    "This prints a passed string into this function"
+    print(str)
+    return
+
 
 # Now you can call printme function
-printme( str = "My string")
+printme(str="My string")
+
 
 # Other Function definition is here -----------------------------
-def printinfo( name, age ):
-   "This prints a passed info into this function"
-   print ("Name: ", name)
-   print ("Age ", age)
-   return
+def printinfo(name, age):
+    "This prints a passed info into this function"
+    print("Name: ", name)
+    print("Age ", age)
+    return
+
 
 # Now you can call printinfo function
-printinfo( age = 50, name = "miki" ) # llamado por keyword consiste en enviar los valores desde el llamado de la función
-                                     # El orden como son enviados los parámetros no importa
-                                     # El nombre de la variable debe ser el mismo que recibe la función (age, miki).
+# llamado por keyword consiste en enviar los valores desde el llamado de la función
+# El orden como son enviados los parámetros no importa
+# El nombre de la variable debe ser el mismo que recibe la función (age, miki).
+    printinfo(age=50, name="miki")
