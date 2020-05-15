f11564d07c8366be82ff02f949255e8fc7f2405b
Alfonso AG <alfareiza@gmail.com>
2020-05-15 20:44:36 -0300

    Removing details of dependencies while o build

--- Examples/Function_examples/pass_by_reference_no_return.py
+++ Examples/Function_examples/pass_by_reference_no_return.py
@@ -1,11 +1,15 @@
+mylist = [10, 20, 30]
+
+
 # Function definition is here
-def changeme( mylist ):
-   "This changes a passed list into this function"
-   mylist = [1,2,3,4] # This would assi new reference in mylist
-   print ("Values inside the function: ", mylist)
-   return myList#como aqui no retorna nada, myList solo cambia dentro de la función
+def changeme(mylist):
+    "This changes a passed list into this function"
+    mylist = [1, 2, 3, 4]  # This would assi new reference in mylist
+    print("Values inside the function: ", mylist)
+    # Como aqui no retorna nada, myList solo cambia dentro de la función
+    pass
+
 
 # Now you can call changeme function
-mylist = [10,20,30]
-changeme( mylist )
-print ('Values outside the function: ', mylist)
+changeme(mylist)
+print('Values outside the function: ', mylist)
