f3bda558d1ba13ed7920747a0b7fc61c92f913f2
Alfonso AG <alfareiza@gmail.com>
2020-05-15 20:44:36 -0300

    Removing details of dependencies while o build

--- Examples/logicals_conditions_examples/all_any.py
+++ Examples/logicals_conditions_examples/all_any.py
@@ -1,28 +1,34 @@
-#https://pythonhelp.wordpress.com/2012/11/06/all-e-any/
+# https://pythonhelp.wordpress.com/2012/11/06/all-e-any/
+
 
 listaDeInteiros = [2, 6, 4, 7, -2]
+
+
 def using_all():
     listaDeInteiros = [2, 6, 4, 7, -2]
-    if all(i % 2 == 0 for i in listaDeInteiros): #Verifica si todos los elementos en la lista CUMPLEN una condición
-        print ('Todos sao pares')
+    if all(i % 2 == 0 for i in listaDeInteiros):  # Verifica si todos los elementos en la lista CUMPLEN una condición
+        print('Todos sao pares')
     else:
-        print ('Tem algum impar')
+        print('Tem algum impar')
     return
+
 
 def using_any():
-    if not any(i % 2 != 0 for i in listaDeInteiros): #Verifica si todos los elementos en la lista NO CUMPLEN una condición
-        print ('Todos sao pares')
+    # Verifica si todos los elementos en la lista NO CUMPLEN una condición
+    if not any(i % 2 != 0 for i in listaDeInteiros):
+        print('Todos sao pares')
     else:
-        print ('Tem algum impar')
+        print('Tem algum impar')
     return
+
 
 def usingall_2():
-    lista_de_strings = ['Alfonso','Areiza','Guerra','30 años']
+    lista_de_strings = ['Alfonso', 'Areiza', 'Guerra', '30 años']
     if all(s != '' for s in lista_de_strings):
-        print ("funcao_que_nao_trata_strings_vazias(lista_de_strings)")
+        print("funcao_que_nao_trata_strings_vazias(lista_de_strings)")
     else:
-        print ("Erro: strings vazias existem na lista!")
+        print("Erro: strings vazias existem na lista!")
     return
 
-usingall_2()
 
+usingall_2()
