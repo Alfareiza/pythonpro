42b83da592d36e936aecf8e55d2282ecd6e70099
Alfonso AG <alfareiza@gmail.com>
2020-05-15 15:55:18 -0300

    verify if it is consonant or vowel

--- Exercises/vowel_ou_consonant.py
+++ Exercises/vowel_ou_consonant.py
@@ -2,8 +2,9 @@
 This program receive a letter and verify if it is consonant or vowel.
 """
 letra = input('Digite uma Letra -> ')
+
 def vocal_ou_consoante(letra):
-    if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+    if letra in set('aeiou'):
         return 'Used digitó una vocal'
     else:
         return 'Usted digitó una consonante'
