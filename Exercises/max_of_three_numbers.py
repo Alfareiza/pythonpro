6c5b52454ec7b59da384337a04b85b18731f6799
Alfonso AG <alfareiza@gmail.com>
2020-05-16 15:55:12 -0300

    Highest number of a tuple

--- Exercises/max_of_three_numbers.py
+++ Exercises/max_of_three_numbers.py
@@ -1,6 +1,8 @@
 """
 Highest number of a tuple
 """
+
+
 def max_of_3_numbers_op1(args):
     num_anterior = args[0]
     for valor in args:
@@ -10,11 +12,11 @@ def max_of_3_numbers_op1(args):
 
     return maxnumber
 
+
 def max_of_3_numbers_op2(args):
     return max(args)
 
-tpl = (1,5,4,10,7,22)
+
+tpl = (1, 5, 4, 10, 7, 22)
 print(max_of_3_numbers_op1(tpl))
 print(max_of_3_numbers_op2(tpl))
-
-
