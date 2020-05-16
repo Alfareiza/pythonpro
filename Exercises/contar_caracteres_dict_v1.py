2b159a6a05d39a2106e5db46b7de3eef8c4ada7d
Alfonso AG <alfareiza@gmail.com>
2020-05-16 16:31:03 -0300

    Count quantity of characters in a string, returning a dict.

--- Exercises/contar_caracteres_dict_v1.py
+++ Exercises/contar_caracteres_dict_v1.py
@@ -4,23 +4,25 @@ This program count the quantity of every characters in a string returning the re
  >>>contar_caracteres('Alfonso')
  {'f':1,'l':1,'n':1,'o':2,'s':1}
 """
-def contar_caracteres(s):
 
-    caracteres_ordenados = sorted(s.lower()) #caracteres_ordenados = ['a','f','l','n','o','o','s']
-    caracter_anterior = caracteres_ordenados[0] #caracter_anterior = ['a']
+
+def contar_caracteres(s):
+    caracteres_ordenados = sorted(s.lower())  # Caracteres_ordenados = ['a','f','l','n','o','o','s']
+    caracter_anterior = caracteres_ordenados[0]  # Caracter_anterior = ['a']
     contagem = 1
-    resultado ={}
+    resultado = {}
     for caracter in caracteres_ordenados[1::]:
         if caracter == caracter_anterior:
             contagem += 1
         else:
-            resultado [caracter_anterior] = contagem
+            resultado[caracter_anterior] = contagem
             caracter_anterior = caracter
             contagem = 1
 
-    resultado [caracter_anterior] = contagem
+    resultado[caracter_anterior] = contagem
 
     return resultado
+
 
 if __name__ == '__main__':
     print(contar_caracteres('Alfonso'))
