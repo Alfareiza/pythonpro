8682979695f63d03ba47cc742c13cccdfda9b138
Alfonso AG <alfareiza@gmail.com>
2020-05-16 14:29:53 -0300

    Testing request library using api github

--- Exercises/requests/api_git_hub.py
+++ Exercises/requests/api_git_hub.py
@@ -4,6 +4,7 @@ This program gets a photo of a username in github using requests library.
 
 import requests
 
+
 def get_avatar(usuario):
     """
     get a avatar of a user in github
@@ -13,6 +14,7 @@ def get_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     response = requests.get(url)
     return response.json()['avatar_url']
+
 
 if __name__ == '__main__':
     username = input('Digite su username in github : ')
