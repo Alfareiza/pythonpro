1680b0249e042269e866dca295f38f3d4f0c3462
Alfonso AG <alfareiza@gmail.com>
2020-05-16 16:31:45 -0300

    Count quantity of characters in a string, returning a list.

--- Exercises/contagem_de_caracteres_lista.py
+++ Exercises/contagem_de_caracteres_lista.py
@@ -1,8 +1,9 @@
 """
 This program count the quantity of every characters in a string
 """
-def contar_caracteres(s):
 
+
+def contar_caracteres(s):
     caracteres_ordenados = sorted(s)
     caracter_anterior = caracteres_ordenados[0]
     contagem = 1
@@ -16,6 +17,7 @@ def contar_caracteres(s):
             contagem = 1
 
     print(f'{caracter_anterior}:{contagem}')
+
 
 if __name__ == '__main__':
     contar_caracteres('Alfonso')
