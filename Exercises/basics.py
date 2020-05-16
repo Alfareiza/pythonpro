86b0d8cac3c088213a0e516199786a9eb161f44b
Alfonso AG <alfareiza@gmail.com>
2020-05-16 16:44:06 -0300

    My Firts contact with python

--- Exercises/basics.py
+++ Exercises/basics.py
@@ -10,6 +10,7 @@ def inverseName():
     name = ' '.join(nameList)
     print('Su Nombre y Apellido Al revés son:\n', name)
 
+
 def generateTupleandList():
     """
     This program receive a sequence of comma-separated numbers from user and
@@ -18,27 +19,29 @@ def generateTupleandList():
     numbers = '1,2,3,4,5'
     numbersList = numbers.split(',')
     numbersTuple = tuple(numbersList)
-    print('This is a List = ',numbersList,'\n','This is Tuple = ',numbersTuple)
+    print('This is a List = ', numbersList, '\n', 'This is Tuple = ', numbersTuple)
 
-def  extensionoffile():
+
+def extensionoffile():
     """
     This function accept a filename from the user and print the extension of that.
     :return:
     """
     filename = 'abc.java'
     filenameTemp = filename.split('.')
-    print('Your file extension is : ',repr(filenameTemp[-1]))
+    print('Your file extension is : ', repr(filenameTemp[-1]))
+
 
 def firstandlastelementList():
     """
     This function return the first and last colors from a List
-    :return: 
+    :return: string
     """
     color_list = ["Red", "Green", "White", "Black"]
-    print('El Primer color es el :',repr(color_list[0]),'\nEl Último color es el :',repr(color_list[-1]))
+    print('El Primer color es el :', repr(color_list[0]), '\nEl Último color es el :', repr(color_list[-1]))
+
 
 inverseName()
 generateTupleandList()
 extensionoffile()
 firstandlastelementList()
-
