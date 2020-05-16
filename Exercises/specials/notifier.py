a1b9bb9fb177bf9ac9b185162038684b63298953
Alfonso AG <alfareiza@gmail.com>
2020-05-16 14:28:25 -0300

    Learning to use this lib what sends windows notification

--- Exercises/specials/notifier.py
+++ Exercises/specials/notifier.py
@@ -4,4 +4,4 @@ Learning to use this lib what sends windows notification
 from win10toast import ToastNotifier
 
 toast = ToastNotifier()
-toast.show_toast("Notification", "Lorem Ipsum", icon_path=None,duration= 20)
+toast.show_toast("Notification", "Lorem Ipsum", icon_path=None, duration=20)
