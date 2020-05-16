c2d774b8176fc6bb292e4c72d8ada626ecce734d
Alfonso AG <alfareiza@gmail.com>
2020-05-16 15:31:42 -0300

    Given a number, it is reduced to zero by certain logic counting the steps

--- Exercises/steps_to_reduce_a_number_to_zero.py
+++ Exercises/steps_to_reduce_a_number_to_zero.py
@@ -1,5 +1,6 @@
 """
-Given a number, is calculated it is reduced to zero by certain logic. In addition the steps are counted until reaching zero
+Given a number, is calculated it is reduced to zero by certain logic. In addition the steps
+are counted until reaching zero
 Input: num = 14
 Output: 6
 Explanation:
@@ -10,15 +11,16 @@ Step 4) 3 es impar; subtract 1 and obtain 2.
 Step 5) 2 es par; divide by 2 and obtain 1.
 Step 6) 1 es impar; subtract 1 and obtain 0."""
 
-def numberOfSteps (num):
+
+def numberOfSteps(num):
     """
     :type num: int
     :rtype: int
     """
     count = 1
     while num != 0:
-        if num%2 == 0:
-            print (f'Step {count}. {int(num)} es par; dividido por 2 se obtiene el {int(num/2)}.')
+        if num % 2 == 0:
+            print(f'Step {count}. {int(num)} es par; dividido por 2 se obtiene el {int(num/2)}.')
             num = num/2
             count += 1
         else:
@@ -26,6 +28,7 @@ def numberOfSteps (num):
             num = num - 1
             count += 1
     return
+
 
 print('↓ Digita un Número Positivo ↓')
 num = input()
