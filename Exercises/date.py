96501d897741bc151e83bd56e2cb5773eb69bb23
Alfonso AG <alfareiza@gmail.com>
2020-05-16 16:25:25 -0300

    Learning to recognize the library datetime.

--- Exercises/date.py
+++ Exercises/date.py
@@ -5,25 +5,25 @@ from datetime import datetime
 import locale
 
 # Idioma "es-ES" (código para el español de España)
-locale.setlocale(locale.LC_ALL, 'es-CO') # Lo sgte no vendria en ingles a no ser por esta linea 5
-#print(locale.setlocale.__doc__) De que trata locale?
+locale.setlocale(locale.LC_ALL, 'es-CO')  # Lo sgte no vendria en ingles a no ser por esta linea 5
+# print(locale.setlocale.__doc__) De que trata locale?
 dt = datetime.now()
-print(dt.strftime("%Y-%m-%d %H:%M:%S")) #strftime es la formatação
-print(dt.strftime("%A %d %B %Y %I:%M")) #strftime es la formatação // %I 12h - %H 24h
+print(dt.strftime("%Y-%m-%d %H:%M:%S"))  # strftime es la formatação
+print(dt.strftime("%A %d %B %Y %I:%M"))  # strftime es la formatação // %I 12h - %H 24h
 print(dt.strftime("%A %d de %B del %Y - %H:%M"))
 
 print(dt)
-print('año',dt.year)         # año
-print('mes',dt.month)        # mes
-print('day',dt.day)          # día
+print('año', dt.year)         # año
+print('mes', dt.month)        # mes
+print('day', dt.day)          # día
 
-print('hora',dt.hour)         # hora
-print('minutos',dt.minute)       # minutos
-print('segundos',dt.second)       # segundos
-print('microsegundos',dt.microsecond)  # microsegundos
+print('hora', dt.hour)         # hora
+print('minutos', dt.minute)       # minutos
+print('segundos', dt.second)       # segundos
+print('microsegundos', dt.microsecond)  # microsegundos
 
 print("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
 print("{}/{}/{}".format(dt.day, dt.month, dt.year))
 
-dt = datetime(2020,5,25) #Creando dates
+dt = datetime(2020, 5, 25)  # Creando dates
 print(dt.strftime("%A %d de %B del %Y - %H:%M"))
