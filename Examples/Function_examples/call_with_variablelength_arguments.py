425d1a22fdd7e760aab35630410cc08d9ba2d5ba
Alfonso AG <alfareiza@gmail.com>
2020-05-16 14:13:11 -0300

    How is called a function with variable lenght arguments

--- Examples/Function_examples/call_with_variablelength_arguments.py
+++ Examples/Function_examples/call_with_variablelength_arguments.py
@@ -1,23 +1,25 @@
-# Function definition is here known as 'Parámetros Variables en Funciones' ----------------
+#  Function definition is here known as 'Parámetros Variables en Funciones' ----------------
 def printinfo(arg1, *vartuple):
     "This prints a variable passed arguments"
-    print("Output arg is: ",arg1)
+    print("Output arg is: ", arg1)
 
     for var in vartuple:
-        print("vartuple: ",var)
+        print("vartuple: ", var)
     return
 
-# Now you can call printinfo function
-#printinfo(10)
+
+#  Now you can call printinfo function
+#  printinfo(10)
 printinfo(70, 60, 50)
 
-#'Parámetros Variables en Funciones' ----------------
+
+# Parámetros Variables en Funciones
 def sum(self, *args):
     aux = 0
     for valor in args:
-        aux= aux + valor
-    print('Resultado : ',aux)
+        aux = aux + valor
+    print('Resultado : ', aux)
     return
 
-sum(1,10)
 
+sum(1, 10)
