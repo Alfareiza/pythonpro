d00524e0a13bad4287c136db6f85b9afc3b48634
Alfonso AG <alfareiza@gmail.com>
2020-05-21 19:40:58 -0300

    Configuration of pytest

--- pythonpro/tests/test_github_api.py
+++ pythonpro/tests/test_github_api.py
@@ -4,15 +4,14 @@ from pythonpro import api_git_hub
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars3.githubusercontent.com/u/63620799?v=4'
     resp_mock.json.return_value = {'login': 'Alfareiza', 'id': 63620799,
                                    'avatar_url': url}
-    get_original = api_git_hub.requests.get
-    api_git_hub.requests.get = Mock(return_value=resp_mock)
-    yield url
-    api_git_hub.requests.get = get_original
+    get_mock = mocker.patch('pythonpro.api_git_hub.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
