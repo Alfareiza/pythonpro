d1b62d1847f1f38ac36d03965731ea5256b2b3d3
Alfonso AG <alfareiza@gmail.com>
2020-10-03 19:40:26 -0300

    Giving a file with consume in bytes for users, generate a file report, ordered by bytes consume

--- Exercises/files/users_free_space_report.py
+++ Exercises/files/users_free_space_report.py
@@ -1,6 +1,6 @@
 """
 https://wiki.python.org.br/ExerciciosArquivos
-2. A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos.
+2. A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de files.
  Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, e
  identificar os usuários com maior espaço ocupado. Através de um programa, baixado da Internet, ele conseguiu gerar
   o seguinte arquivo, chamado "usuarios.txt":
